de06f93b9ddc4c21937d5733b96f4cff6dc63c64
Abeer Kapoor <abrkapoor@gmail.com>
2025-08-04 20:27:16 +0530

    insertion sort

--- serach, sort/InsertionSort.java
+++ serach, sort/InsertionSort.java
@@ -13,9 +13,25 @@ public class InsertionSort {
         }
     }
 
+    public static void Insertion(int [] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int j = i;
+            while(j>0 && arr[j-1] > arr[j]){
+                swap(arr, j, j-1);
+                j--;
+            }
+        }
+    }
+
+    public static void swap(int [] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     public static void main(String[] args) {
         int [] arr = {5,3,8,1,2,0};
-        inertionSort(arr);
+        Insertion(arr);
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i] + " ");
         }    
