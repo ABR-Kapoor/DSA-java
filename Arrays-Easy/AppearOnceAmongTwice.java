a20fece2583c43ac758bca4f78a30aaa4fb3852d
Abeer Kapoor <abrkapoor@gmail.com>
2025-08-16 19:46:23 +0530

    better and optimum approach to find the one number that appear once among the other twice

--- Arrays-Easy/AppearOnceAmongTwice.java
+++ Arrays-Easy/AppearOnceAmongTwice.java
@@ -13,8 +13,32 @@ public class AppearOnceAmongTwice {
         return -1;
     }
 
+    public static int BetterAppearOnce(int[] arr) {
+        int n = arr.length;
+        int[] hash = new int[n];
+        for (int i = 0; i < n; i++) {
+            hash[arr[i]]++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (hash[i] == 1)
+                return i;
+        }
+        return -1;
+    }
+
+    public static int OptiAppearOnce(int[] arr) {
+        int n = arr.length;
+        int xor = 0;
+        for (int i = 0; i < n; i++) {
+            xor ^= arr[i];
+        }
+        return xor;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 2, 3, 1, 3, 1, 2, 4 };
         System.out.println(appearOnce(arr));
+        System.out.println(BetterAppearOnce(arr));
+        System.out.println(OptiAppearOnce(arr));
     }
 }
