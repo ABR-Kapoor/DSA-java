892eda3bf5d18af5f18524843d08c287ce9f9b95
Abeer Kapoor <abrkapoor@gmail.com>
2025-09-07 21:29:52 +0530

    first DP question, Buying and Selling stock

---
+++ DP/StockBuySell.java
@@ -0,0 +1,15 @@
+package DP;
+
+public class StockBuySell {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int profit = 0;
+        int min = prices[0];
+        for (int i = 0; i<n; i++){
+            int cost = prices[i] - min;
+            profit = Math.max(profit, cost);
+            min = Math.min(min, prices[i]);
+        }
+        return profit;
+    }
+}
