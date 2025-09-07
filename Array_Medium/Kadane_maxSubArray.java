123e00b9f26ca81e4bb841c77e6b6c35b280af2f
Abeer Kapoor <abrkapoor@gmail.com>
2025-09-07 19:38:09 +0530

    max subarray better

--- Array_Medium/Kadane_maxSubArray.java
+++ Array_Medium/Kadane_maxSubArray.java
@@ -1,24 +1,42 @@
 package Array_Medium;
 
 public class Kadane_maxSubArray {
-    public static int k(int[] arr){
+    public static int k(int[] arr) {
         int n = arr.length;
         int max = Integer.MIN_VALUE;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
                 int sum = 0;
-                for(int k=i; k<=j; k++){
+                for (int k = i; k <= j; k++) {
                     sum += arr[k];
+                    if (sum > max)
+                        max = sum;
                 }
-                if(sum > max) max = sum;
             }
         }
         return max;
     }
-    public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int result = k(arr);
-        System.out.println("Maximum subarray sum is: " + result);
+
+    public static int Kb(int[] arr) {
+        int n = arr.length;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum > max)
+                    max = sum;
+            }
+        }
+        return max;
     }
-    
+
+    public static void main(String[] args) {
+        int[] arr = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        int result = k(arr);
+        int result2 = Kb(arr);
+        System.out.println("Maximum subarray sum is: " + result);
+        System.out.println("Maximum subarray sum is: " + result2);
+    }
+
 }
