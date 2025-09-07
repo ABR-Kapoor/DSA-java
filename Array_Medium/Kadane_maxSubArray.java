d3fdeca17d86f6a009d2545e9ac4e36c96c1890d
Abeer Kapoor <abrkapoor@gmail.com>
2025-09-07 19:41:11 +0530

    Optimal Soultion Kadane's Algo

--- Array_Medium/Kadane_maxSubArray.java
+++ Array_Medium/Kadane_maxSubArray.java
@@ -17,6 +17,18 @@ public class Kadane_maxSubArray {
         return max;
     }
 
+    public static int KO(int [] arr){
+        int n = arr.length;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i =0; i<n; i++){
+            sum += arr[i];
+            if (sum > max) max = sum;
+            if (sum < 0) sum = 0;
+        }
+        return max;
+    }
+
     public static int Kb(int[] arr) {
         int n = arr.length;
         int max = Integer.MIN_VALUE;
@@ -35,6 +47,8 @@ public class Kadane_maxSubArray {
         int[] arr = { -2, -3, 4, -1, -2, 1, 5, -3 };
         int result = k(arr);
         int result2 = Kb(arr);
+        int result3 = KO(arr);
+        System.out.println("Maximum subarray sum is: " + result3);
         System.out.println("Maximum subarray sum is: " + result);
         System.out.println("Maximum subarray sum is: " + result2);
     }
