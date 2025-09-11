bd4e07ec8d69b5966ffbb28fd7b507524ed545d2
Abeer Kapoor <abrkapoor@gmail.com>
2025-09-11 09:24:48 +0530

    Leader in an array : Optimise

--- Array_Medium/LeaderInAnArray.java
+++ Array_Medium/LeaderInAnArray.java
@@ -1,6 +1,7 @@
 package Array_Medium;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class LeaderInAnArray{
     public static ArrayList<Integer> B(int [] arr){
@@ -15,10 +16,29 @@ public class LeaderInAnArray{
         }
         return ansList;
     }
+    public static ArrayList<Integer> Op(int [] arr){
+        int n = arr.length;
+        ArrayList <Integer> ans = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        for (int i = n-1; i >=0; i--) {
+            if(arr[i] >= max){
+                ans.add(arr[i]);
+                max = arr[i];
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
     public static void main(String[] args) {
-        int [] arr = {10,22,12,3,0,6};
+        int [] arr = {61,61,17};
         ArrayList<Integer> leaders = B(arr);
+        ArrayList<Integer> optimizedLeaders = Op(arr);
+        System.out.println("Leaders in the array (Brute Force):");
         for (int leader : leaders) {
+            System.out.print(leader + " ");
+        }
+        System.out.println("\nLeaders in the array (Optimized):");
+        for (int leader : optimizedLeaders) {
             System.out.print(leader + " ");
         }
     }
