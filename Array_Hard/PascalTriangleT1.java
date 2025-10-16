aa1f2cd3c576c5c917bf103f6bcfb7bd54709647
Abeer Kapoor <abrkapoor@gmail.com>
2025-10-16 09:09:26 +0530

    Array hard started, Pascal's triangle type 1

---
+++ Array_Hard/PascalTriangleT1.java
@@ -0,0 +1,21 @@
+package Array_Hard;
+
+public class PascalTriangleT1 {
+    public static long nCr(int n, int r){
+        long res =1;
+        for (int i =0; i<r; i++){
+            res = res * (n-i);
+            res = res / (i+1);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i =0; i<n; i++){
+            for (int j =0; j<=i; j++){
+                System.out.print(nCr(i,j)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
