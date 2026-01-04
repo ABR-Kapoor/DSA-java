17db3ef835e5d35ccd1ed6c0a44e9e82d23a1b0f
Abeer Kapoor <101738668+ABR-Kapoor@users.noreply.github.com>
2026-01-04 14:18:25 +0530

    Add main function to test OptiIntersection

--- Arrays-Easy/IntersactionSortedArr.java
+++ Arrays-Easy/IntersactionSortedArr.java
@@ -36,6 +36,8 @@ public class IntersactionSortedArr {
         return ans;
     }
 
+    // main function
+
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {3, 4, 5, 6, 7};
